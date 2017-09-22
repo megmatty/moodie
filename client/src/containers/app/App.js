9a0dd5f44bb003dedcf80f08b515268ff912e4d1
Tzikas <niko.tzikas@gmail.com>
2017-09-22 19:52:36 -0400

    got react d3 recharts

--- client/src/containers/app/App.js
+++ client/src/containers/app/App.js
@@ -3,19 +3,23 @@ import { Route, Link } from 'react-router-dom'
 import Home from '../home/Home';
 import About from '../about/About';
 import Dashboard from '../dashboard/Dashboard';
+import Stock from '../stock/Stock';
 
 const App = () => (
   <div>
     <header>
       <Link to="/">Home</Link><br />
-      <Link to="/about-us">About</Link><br />
-      <Link to="/dashboard">Dashboard</Link>
+      <Link to="/about-us">Entries</Link><br />
+      <Link to="/dashboard">Dashboard</Link><br />
+      <Link to="/stock">Stock</Link>
+      
     </header>
 
     <main>
       <Route exact path="/" component={Home} />
       <Route exact path="/about-us" component={About} />
       <Route exact path="/dashboard" component={Dashboard} />
+      
     </main>
   </div>
 )
