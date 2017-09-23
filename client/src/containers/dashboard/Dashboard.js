d979be1efe999a5f1a0a5cddd48e8d875c2ba477
Tzikas <niko.tzikas@gmail.com>
2017-09-23 16:57:53 -0400

    cool

--- client/src/containers/dashboard/Dashboard.js
+++ client/src/containers/dashboard/Dashboard.js
@@ -4,9 +4,9 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
 import { findAll } from '../../entries/newentries';
-//import { LineChart, Line } from 'recharts';
-//import {Recharts} from 'recharts';
 import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+import { PieChart, Pie, Sector, Cell } from 'recharts';
+
 const data = [
       {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
       {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
@@ -16,6 +16,48 @@ const data = [
       {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
       {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
 ];
+
+
+
+
+
+const pieData = [{name: 'Group A', value: 400}, {name: 'Group B', value: 300},
+                  {name: 'Group C', value: 300}, {name: 'Group D', value: 200}];
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
+const RADIAN = Math.PI / 180;                    
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+ 	const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x  = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy  + radius * Math.sin(-midAngle * RADIAN);
+ 
+  return (
+    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} 	dominantBaseline="central">
+    	{`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
+
+const SimplePieChart = React.createClass({
+	render () {
+  	return (
+    	<PieChart width={800} height={400} onMouseEnter={this.onPieEnter}>
+        <Pie
+          data={pieData} 
+          cx={300} 
+          cy={200} 
+          labelLine={false}
+          label={renderCustomizedLabel}
+          outerRadius={80} 
+          fill="#8884d8"
+        >
+        	{
+          	data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>)
+          }
+        </Pie>
+      </PieChart>
+    );
+}})
 
 class Dashboard extends Component {
 	constructor(props) {
@@ -51,6 +93,7 @@ class Dashboard extends Component {
        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
       </LineChart>
 			
+      <SimplePieChart />
 			
 			  <h1>Dashboard</h1>
 			  <div>
